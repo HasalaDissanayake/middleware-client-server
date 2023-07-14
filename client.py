88b5b7652ac23268fb1ed2e47a04d7682157687d
Hasala <hasaladissanayake@yahoo.com>
2023-07-14 17:53:47 +0530

    modifications to existing code

--- client.py
+++ client.py
@@ -1,47 +1,29 @@
 import socket
 import sys
 
-PORT = 5000
-HOST = socket.gethostbyname(socket.gethostname())
 
+def start_client(server_ip, port):
+    client_socket = socket.socket()
+    client_socket.connect((server_ip, port))
 
-def client_start(server, port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ADDR = (server, port)
-    client_socket.connect(ADDR)
-    client_address = client_socket.getsockname()
+    print(f"Connected to server {server_ip} on {port}")
 
-    message = input(f"[CLIENT {client_address}]: ")
+    while True:
+        message = input("Enter a message (type 'terminate' to exit): ")
+        client_socket.send(message.encode())
+        if message == "terminate":
+            break
 
-    while message.lower().strip() != 'terminate':
-        if message.strip():
-            client_socket.send(message.encode())
-            reply = client_socket.recv(1024).decode()
-            print("[FROM SERVER]: " + reply)
+    print("Disconnected from server")
 
-            message = input(f"[CLIENT {client_address}]: ")
-        else:
-            message = input(f"[CLIENT {client_address}]: ")
-    
-    print(f"[SESSION {client_address} TERMINATED]")
     client_socket.close()
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("FORMAT: python client.py SERVER_IP SERVER_PORT")
-        sys.exit(0)
+        print("FORMAT: python client.py SERVER_IP PORT")
+        sys.exit(1)
 
-    input_server = sys.argv[1]
-    input_port = int(sys.argv[2])
-
-    if(input_port != PORT):
-        print("Invalid Server Port Given")
-        sys.exit(0)
-
-    if(input_server != HOST):
-        print("Invalid Server IP Given")
-        sys.exit(0)
-
-
-    client_start(input_server, input_port)
+    server_ip = sys.argv[1]
+    port = int(sys.argv[2])
+    start_client(server_ip, port)
--- server.py
+++ server.py
@@ -1,56 +1,38 @@
 import socket
-import threading
 import sys
+import threading
 
-PORT = 5000
-HOST = socket.gethostbyname(socket.gethostname())
-
-
-def server_start(port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ADDR = (HOST, port)
-    server_socket.bind(ADDR)
-    print("[STARTING] server is starting...")
+def start_server(port):
+    server_socket = socket.socket()
+    server_socket.bind(('localhost', port))
     server_socket.listen()
-    print(f"[LISTENING] Server is listening on {HOST}")
-
-    while True:
-        conn, address = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,address))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-
-
-def handle_client(conn, address):
-    print(f"[NEW CONNECTION] {address} connected..")
-
-    while True:
-        message = conn.recv(1024).decode()
-        if not message:
-            break
-        print(f"[FROM {address}]: " + message)
-
-        reply = input("[FROM SERVER]: ")
-        
-        while not reply.strip():
-            reply = input("[FROM SERVER]: ")
-
-        conn.send(reply.encode())
-  
     
-    conn.close()
+    print(f"Server started and listening on port {port}")
+    
+    client_socket, client_address = server_socket.accept()
+    thread = threading.Thread(target=handle_client, args=(client_socket,client_address))
+    thread.start()
+    print(f"Active Connections {threading.active_count() - 1}")
+    
 
-
+def handle_client(client_socket, client_address):
+    print(f"Client connected from {client_address}")
+    
+    while True:
+        data = client_socket.recv(1024).decode()
+        if data == "terminate":
+            break
+        print(f"Received from client {client_socket}: {data}")
+    
+    print("Client disconnected")
+    
+    client_socket.close()
+ 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("FORMAT: python server.py PORT")
-        sys.exit(0)
+        sys.exit(1)
 
-    input_port = int(sys.argv[1])
-
-    if(input_port != PORT):
-        print("Invalid Port Given")
-        sys.exit(0)
-
-    server_start(input_port)
+    port = int(sys.argv[1])
+    start_server(port)
